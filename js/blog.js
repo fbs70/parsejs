3aa9d3e4f7efb560caada8ff6f5687a7327c6dd8
Blair <blair.shotlow@gmail.com>
2015-03-30 23:14:48 -0500

    update files

---
+++ js/blog.js
@@ -0,0 +1,14 @@
+$(function() {
+ 
+    Parse.$ = jQuery;
+ 
+    // Replace this line with the one on your Quickstart Guide Page
+    Parse.initialize("bin8MzZfpkdxSQ5rq9L7Iq0gsCh95HgTNBuWwwW7", "R9ACofZwLvdvfizi3T1CbyWLSF0euUFC0X0uQ8RV");
+ 
+    var TestObject = Parse.Object.extend("TestObject");
+    var testObject = new TestObject();
+    testObject.save({foo: "bar"}).then(function(object) {
+      alert("yay! it worked");
+    });
+ 
+});
