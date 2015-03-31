27c61d5b9f35538035a1093f5bf6938b43b0df05
Blair <blair.shotlow@gmail.com>
2015-03-31 07:18:24 -0500

    update js

--- js/blog.js
+++ js/blog.js
@@ -5,10 +5,32 @@ $(function() {
     // Replace this line with the one on your Quickstart Guide Page
     Parse.initialize("bin8MzZfpkdxSQ5rq9L7Iq0gsCh95HgTNBuWwwW7", "R9ACofZwLvdvfizi3T1CbyWLSF0euUFC0X0uQ8RV");
  
-    var TestObject = Parse.Object.extend("TestObject");
-    var testObject = new TestObject();
-    testObject.save({foo: "bar"}).then(function(object) {
-      alert("yay! it worked");
-    });
+	var Blog = Parse.Object.extend("Blog");
+	
+	var Blogs = Parse.Collection.extend({
+	    model: Blog
+	});
+	
+	var blogs = new Blogs();
+	
+	blogs.fetch({
+		success: function(blogs) {
+		    var blogsView = new BlogsView({ collection: blogs });
+		    blogsView.render();
+		    $('.main-container').html(blogsView.el);
+		},
+	    error: function(blogs, error) {
+	        console.log(error);
+	    }
+	});
+
+	var BlogsView =  Parse.View.extend({
+	    template: Handlebars.compile($('#blogs-tpl').html()),
+	    render: function(){
+	        var collection = { blog: this.collection.toJSON() };
+	        this.$el.html(this.template(collection));
+	    }
+	});
+	
  
 });
