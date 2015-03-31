53f9001f93c0ed83f4c75052163e01570997b3c1
Blair <blair.shotlow@gmail.com>
2015-03-31 07:36:12 -0500

    add admin code

---
+++ js/admin.js
@@ -0,0 +1,8 @@
+$(function() {
+ 
+    Parse.$ = jQuery;
+ 
+    // Replace this line with the one on your Quickstart Guide Page
+    Parse.initialize("bin8MzZfpkdxSQ5rq9L7Iq0gsCh95HgTNBuWwwW7", "R9ACofZwLvdvfizi3T1CbyWLSF0euUFC0X0uQ8RV");
+ 
+});
